6fc96e3c64580f956f0be566aedc6c50b256d728
Anthony Morgan <morganaj@ucmail.uc.edu>
2020-11-10 18:34:35 -0500

    move project files to cloned repository

---
+++ game.js
@@ -0,0 +1,79 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+let deck=[];
+
+//will grab currency specified from local storage
+let currency="$";
+
+var game = new Phaser.Game(config);
+
+    function preload ()
+    {
+      
+        this.load.image('tabletop', 'assets/tabletop.png');
+        this.load.image('cardback', 'assets/redback.png');
+        freshDeck();
+        deck.forEach(element => {
+            this.load.image(element, 'assets/' + element + '.png');
+        });
+       
+      //  this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
+    }
+
+    function create ()
+    {
+        this.add.image(400, 300, 'tabletop');
+        this.add.image(120, 150, 'cardback');
+
+        deck=shuffleDeck(deck);
+      //  deck.forEach(element =>{
+       // console.log(element);
+      //  });
+    //    this.add.image(140, 170, '2C');
+     //   this.add.image(140, 370, '2D');
+    }
+
+    function update ()
+    {
+
+    }
+
+    function freshDeck(){
+        //setup deck array
+        var suits = ["C","D","H","S"];
+        var pips=["2","3","4","5","6","7","8","9","10","J","Q","K","A"];
+        for(x=0;x<pips.length;x++){
+            for(i=0;i < suits.length;i++){
+                deck.push(pips[x] + suits[i]);
+            }
+        }
+    }
+
+    function shuffleDeck(array){
+        var currentIndex = array.length, temporaryValue, randomIndex;
+
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
+      
+          // Pick a remaining element...
+          randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex -= 1;
+      
+          // And swap it with the current element.
+          temporaryValue = array[currentIndex];
+          array[currentIndex] = array[randomIndex];
+          array[randomIndex] = temporaryValue;
+        }
+      
+        return array;
+      
+    }
